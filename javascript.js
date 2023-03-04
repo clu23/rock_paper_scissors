5db453b4b135a386156f064afbda37334f145ab7
clu23 <clement.lulurga@orange.fr>
2023-03-04 15:33:34 +0100

    finalizing the code of the rock, paper, scissors game in the javascript file

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-let playerScore = 0
-let computerScore = 0
 let roundWinner = ''
 
 function getComputerChoice(){
@@ -11,18 +9,49 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
-    let playerSelection = playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1);
+    playerSelection = playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1);
     if (playerSelection === computerSelection){
-        roundWinner='Tie game !';
+        roundWinner='None';  
     } else if(
         (computerSelection == "Rock" && playerSelection == "Scissors")||
         (computerSelection == "Paper" && playerSelection == "Rock")||
         (computerSelection == "Scissors" && playerSelection == "Paper")
     ){
         roundWinner='Computer';
-        computerScore = ++computerScore;
     } else {
         roundWinner='Player';
-        playerScore=playerScore++;
+    }
+    return(roundWinner);
+  }
+
+
+  function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i=0; i<5; i++){
+        let playerSelection=prompt('Choose your weapon among Rock, Paper and Scissors');
+        playerSelection=playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1);
+        while (!(['Rock','Paper','Scissors'].includes(playerSelection))){
+            playerSelection=prompt("You entered an incorrect string, please choose a weapon among Rock, Paper and Scissors");
+            playerSelection=playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1);
+        }
+        let winner=playRound(playerSelection, getComputerChoice());
+        if (winner==='Player'){
+            playerScore++;
+        }
+        else if(winner='Computer'){
+            computerScore++;
+        }
+        console.log(`Player score: ${playerScore} | Computer Score: ${computerScore}`);
+    }
+    if (computerScore === playerScore){
+        console.log('Tie game !');   
+    } else if (playerScore>computerScore){
+        console.log('Congratulations, You win !');
+    } else {
+        console.log('You lose, computer wins');
     }
   }
+
+
+game()
