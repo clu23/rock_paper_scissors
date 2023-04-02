048e51a2582b24f46304073cb12fae1b164db628
clu23 <clement.lulurga@orange.fr>
2023-04-02 14:35:48 +0200

    pushing initial files for revisited rock,paper,scissors project

--- javascript.js
+++ javascript.js
@@ -1,11 +1,23 @@
-let roundWinner = ''
+let roundWinner=''
+const weapons = document.querySelectorAll(".weapon");
+const listWeapons=['Rock','Paper','Scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     const list_choices=['Rock', 'Paper', 'Scissors'];
     return(list_choices[Math.floor(Math.random()*list_choices.length)])
 }
 
+function animate(e) {
+    console.log(e)
+    e.classList.add('playing');
+  }
 
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
@@ -21,7 +33,6 @@ function playRound(playerSelection, computerSelection) {
     } else {
         roundWinner='Player';
     }
-    return(roundWinner);
   }
 
 
@@ -54,4 +65,52 @@ function playRound(playerSelection, computerSelection) {
   }
 
 
-game()
+
+function clickHandler(){
+    if (playerScore<5 && computerScore<5){
+        animate(this)
+        let playerSelection=listWeapons[this.id-1];
+        playRound(playerSelection, getComputerChoice())
+        if (roundWinner=='Player'){
+            playerScore++;
+        }
+        else if(roundWinner=='Computer'){
+            computerScore++;
+        }
+        if (roundWinner!='None'){
+            if (playerScore>=5||computerScore>=5){
+                if (playerScore>=5){
+                    document.getElementById("display-text").innerHTML = `Congratulations, You win !<br> Player Score : ${playerScore} | Computer Score : ${computerScore}`;
+                }
+                else {
+                    document.getElementById("display-text").innerHTML = `You lose, Computer wins !<br> Player Score : ${playerScore} | Computer Score : ${computerScore}`;
+                }
+            }
+            else{
+                document.getElementById("display-text").innerHTML = `${roundWinner} wins this round<br> Player Score : ${playerScore} | Computer Score : ${computerScore}`;
+            } 
+        }
+        else {
+            document.getElementById("display-text").innerHTML = `Tie game !<br> Player Score : ${playerScore} | Computer Score : ${computerScore}`;
+        }
+        console.log(roundWinner)
+    }
+    else{
+        weapons.forEach(weapon => weapon.removeEventListener('click',clickHandler));
+        if (playerScore>=5){
+            console.log('Player wins !');
+        }
+        else if (computerScore>=5){
+            console.log('Computer wins :(')
+        }
+    }
+}
+
+function game_2(){
+    playerScore = 0;
+    computerScore = 0;
+    weapons.forEach(weapon => weapon.addEventListener('click',clickHandler, false));
+    weapons.forEach(weapon => weapon.addEventListener('transitionend', removeTransition)); 
+}
+
+game_2()
